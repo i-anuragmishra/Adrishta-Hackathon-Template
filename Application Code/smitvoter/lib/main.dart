d4edb37ee9e4c3642b5a2c650b0f0e1bc8d12af3
anurag <anuragmishra@ANURAGs-MacBook-Air.local>
2020-11-29 21:39:31 +0530

    Addded Logo on login page and alligned it

--- Application Code/smitvoter/lib/main.dart
+++ Application Code/smitvoter/lib/main.dart
@@ -6,18 +6,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.amber),
         home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber[500],
-        centerTitle: true,
-        title: Text("SMIT VOTER"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text("data")],
-        ),
-      ),
-    ));
+          appBar: AppBar(
+            backgroundColor: Colors.amber[500],
+            centerTitle: true,
+            title: Text("SMIT VOTER"),
+          ),
+          body: Center(
+            child: SafeArea(
+              top: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage('images/SecurityLogo.jpg')),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
