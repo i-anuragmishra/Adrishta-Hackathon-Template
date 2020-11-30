a5c8a83cedba2c2378059e346c32e9478f135dbc
anurag <anuragmishra@ANURAGs-MacBook-Air.local>
2020-12-01 03:55:53 +0530

    finalised the voting interface and added new data to it

--- Application Code/smitvoter/lib/navigate.dart
+++ Application Code/smitvoter/lib/navigate.dart
@@ -15,42 +15,181 @@ class Navigate extends StatelessWidget {
         ),
         body: Center(
           child: SafeArea(
-            minimum: EdgeInsets.zero,
+            minimum: EdgeInsets.symmetric(horizontal: 8.0),
             top: true,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 SizedBox(
+                  height: 15,
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('images/jess.jpg'),
+                            ),
+                            title: Text('Jessy Pinkman'),
+                            subtitle: Text(
+                                'Screws up everything and but is still loyal'),
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Material(
+                                    color: Colors.amberAccent,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20.0)),
+                                    elevation: 5.0,
+                                    child: MaterialButton(
+                                      onPressed: () {/* ... */},
+                                      minWidth: 100.0,
+                                      height: 30.0,
+                                      child: Text(
+                                        'VOTE',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                        ]),
+                  ),
+                ),
+                SizedBox(
                   height: 10,
                 ),
                 Card(
-                  child:
-                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                    const ListTile(
-                      leading: CircleAvatar(
-                        radius: 10,
-                        backgroundImage: AssetImage('images/SecurityLogo.jpg'),
-                      ),
-                      title: Text('REPRENTATIVE NAME'),
-                      subtitle:
-                          Text('Representative is Standing for the position'),
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                  color: Colors.grey[100],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: MaterialButton(
-                              onPressed: () {/* ... */},
-                              minWidth: 100.0,
-                              height: 30.0,
-                              child: Text(
-                                'Log In',
-                              ),
+                          const ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('images/danny.jpg'),
                             ),
+                            title: Text('Daenerys Targaryen'),
+                            subtitle: Text(
+                                'She is powerfull and authortative vote with caution coz she has dragons '),
                           ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Material(
+                                    color: Colors.amberAccent,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20.0)),
+                                    elevation: 5.0,
+                                    child: MaterialButton(
+                                      onPressed: () {/* ... */},
+                                      minWidth: 100.0,
+                                      height: 30.0,
+                                      child: Text(
+                                        'VOTE',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]),
                         ]),
-                  ]),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('images/walter.jpg'),
+                            ),
+                            title: Text('Walter White'),
+                            subtitle: Text(
+                                'Chemestry is his best suit and uses those skills to get rich'),
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Material(
+                                    color: Colors.amberAccent,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20.0)),
+                                    elevation: 5.0,
+                                    child: MaterialButton(
+                                      onPressed: () {/* ... */},
+                                      minWidth: 100.0,
+                                      height: 30.0,
+                                      child: Text(
+                                        'VOTE',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                        ]),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.grey[100],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('images/snow.jpeg'),
+                            ),
+                            title: Text('John Snow'),
+                            subtitle: Text('Knows Nothing'),
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Material(
+                                    color: Colors.amberAccent,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20.0)),
+                                    elevation: 5.0,
+                                    child: MaterialButton(
+                                      onPressed: () {/* ... */},
+                                      minWidth: 100.0,
+                                      height: 30.0,
+                                      child: Text(
+                                        'VOTE',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                        ]),
+                  ),
                 ),
               ],
             ),
--- Application Code/smitvoter/test/widget_test.dart
+++ Application Code/smitvoter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:smitvoter/login.dart';
+import 'package:smitvoter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
